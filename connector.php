c83dd4df41e8ee8745465779fe8b06970a46a14e
Jediael <65200664+jediael17@users.noreply.github.com>
2025-09-10 14:50:49 -0300

    Update connector.php

--- connector.php
+++ connector.php
@@ -4,11 +4,13 @@ $usuario = 'admin_inventario';
 $senha = 'webserver';
 $dbname = 'dbtutorial';
 $host = '192.168.121.33';
+$port = "3306";
 
 try{
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usuario, $senha);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usuario, $senha, $port);
 }catch(PDOException $e){
     echo $e->getMessage();
 
 }
+
 
